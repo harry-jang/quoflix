a40c7739f69875691b0fac2a6f0603b95c41b3bc
Harry Jang <harry.jang@kakao.com>
2023-09-07 22:04:07 +0900

    애니메이션 추가 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
+import { animate, motion } from "framer-motion"
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,7 +10,7 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
@@ -17,12 +18,16 @@ const Box = styled.div`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-
+const myVars = {
+  start : {scale:0},
+  end: {scale:1, rotateZ: 360, transition: {type:"spring", delay: 0.5}}
+}
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box variants={myVars} initial="start" animate="end" />
+      <motion.div></motion.div>
     </Wrapper>
   );
 }
