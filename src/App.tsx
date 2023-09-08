83136d8665ce797d29af8f7fd83b634a9bc51e6a
Harry Jang <harry.jang@kakao.com>
2023-09-08 22:09:40 +0900

    x축으로 이동함에 따라 스케일이 바뀌는 도형

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from "styled-components";
-import { animate, motion } from "framer-motion"
+import { motion, useMotionValue, useMotionValueEvent, useTransform} from "framer-motion"
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -11,16 +11,6 @@ const Wrapper = styled.div`
 `;
 
 
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
-  /* overflow: hidden; */
-`;
 
 const Box = styled(motion.div)`
   width: 200px;
@@ -30,39 +20,22 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  width: 70px;
-  height: 70px;
-  place-self: center;
-  background-color: white;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const boxVariants = {
-  hover: {scale:1.5, rotateZ: 90},
-  click: {borderRadius:"100px", scale:1},
-  drag: {
-    backgroundColor:"rgb(0, 168, 255)", 
-    transition: {
-      duration: 1
-    }
-  }
-}
 
 
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const scale = useTransform(x, [-800, 0, 800], [2, 1 , 0]);
+
+  useMotionValueEvent(scale, "change", (latest) => {
+    console.log(latest);
+
+  })
+
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box 
-          drag
-          dragSnapToOrigin 
-          dragElastic={0}
-          dragConstraints={biggerBoxRef} 
-          variants={boxVariants} whileHover="hover" whileDrag="drag" whileTap="click" />
-      </BiggerBox>
+        <button onClick={() => x.set(200)}>move</button>
+        <Box style={{x, scale}}
+          drag dragSnapToOrigin/>
     </Wrapper>
   );
 }
