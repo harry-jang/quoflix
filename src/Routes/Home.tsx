b06626c95934f3ac34fc1ff27bd55ecdaaf06349
Harry Jang <harry.jang@kakao.com>
2023-09-10 08:36:34 +0900

    홈화면 슬라이더 추가

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,10 +2,13 @@ import { useQuery } from "react-query";
 import { IGetMoviesResult, getMovies } from "../api";
 import styled from "styled-components";
 import { makeImagePath } from '../utils';
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 
 const Wrapper = styled.div`
     background:black;
+    padding-bottom: 200px;
 `;
 
 const Loader = styled.div`
@@ -36,18 +39,85 @@ const Overview = styled.p`
     width: 50% ;
 `;
 
+const Slider = styled.div`
+    position: relative;
+    top : -100px;
+`;
+
+const Row = styled(motion.div)`
+    display: grid;
+    gap : 5px;
+    grid-template-columns: repeat(6, 1fr);
+    position: absolute;
+    width: 100%
+`;
+
+const Box = styled(motion.div)<{bgPhoto: string}>`
+    background-color: white;
+    background-image: url(${(props) => props.bgPhoto});
+    background-size: cover;
+    background-position: center center;
+    height: 200px;
+    font-size: 66px;
+`;
+
+const rowVariants = {
+    hidden: {
+        x: window.outerWidth+5, // Row의 gap 만큼 보정
+    },
+    visible: {
+        x:0,
+    },
+    exit: {
+        x: -window.outerWidth-5, // Row의 gap 만큼 보정
+    },
+}
+
+const offset = 6;
+
 function Home() {
     const { data, isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
-    console.log(data, isLoading);
+    
+    const [index, setIndex] = useState(0);
+    const increaseIndex = () => {
+        if(data) {
+                if(leaving) return;
+            setLeaving(true);
+            const totalMovies = data.results.length -1; // 1번 영화는 배너로 사용중이기 때문에 1감소
+            const maxIndex = Math.floor(totalMovies / offset) -1; // 페이지가 0에서 시작하기 때문에 1 감소
+            setIndex((prev) => (prev === maxIndex? 0 : prev +1));
+        }
+    };
+    const [leaving, setLeaving] = useState(false);
+    const toggleLeaving = () => setLeaving(prev => !prev);
+
     return <Wrapper>
             {isLoading? (
             <Loader>Loading...</Loader>
             ): (
                 <>
-                    <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+                    <Banner onClick={increaseIndex} bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
                         <Title>{data?.results[0].title}</Title>
                         <Overview>{data?.results[0].overview}</Overview>
                     </Banner>
+                    <Slider>
+                        <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+                            <Row 
+                                variants={rowVariants} 
+                                initial="hidden" 
+                                animate="visible" 
+                                exit="exit"
+                                transition={{type:"tween", duration: 1}}
+                                key={index}>
+                                {data?.results
+                                    .slice(1)
+                                    .slice(offset*index, offset * index + offset)
+                                    .map((movie) => (
+                                    <Box key={movie.id} bgPhoto={makeImagePath(movie.backdrop_path, "w500" )}></Box>
+                                ))}
+                            </Row>
+                        </AnimatePresence>
+                    </Slider>
                 </>
             )}
            </Wrapper>;
